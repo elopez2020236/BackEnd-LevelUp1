bbd675bbad564b08006ca3c5e7040894b2654bcc
Edwar Esaul López Pérez <88805623+elopez2020236@users.noreply.github.com>
2022-08-21 16:34:03 -0600

    Arreglo de errores

--- src/controllers/Formulario.js
+++ src/controllers/Formulario.js
@@ -1,7 +1,6 @@
 const Formulario = require('../models/Formulario.models');
 
 function calcularEdad(fechaNacimiento) {
-
     var fechaActual = new Date();
     var anoActual = parseInt(fechaActual.getFullYear());
     var mesActual = parseInt(fechaActual.getMonth()) + 1;
@@ -21,64 +20,57 @@ function calcularEdad(fechaNacimiento) {
     return edad;
 };
 
-
-
+//----------------------------------------------------------------
 function GenerarFormulario(req, res) {
     var parametros = req.body;
     var modeloFormulario = new Formulario();
     var carnet = parametros.carnet
-    var cantidadLetras = carnet?.length;
-    var PrimeraLetra = carnet?.charAt(0)
-    var TerceraLetra = carnet?.charAt(2)
-    var SextaLetra = carnet.charAt(carnet.length - 1)
-    let fechaNacimientoo = new Date(parametros.fechaNacimiento)
+    var fechaNacimientoo = new Date(parametros.fechaNacimiento)
 
-    modeloFormulario.carnet = parametros.carnet;
-    modeloFormulario.nombre = parametros.nombre;
-    modeloFormulario.direccion = parametros.direccion;
-    modeloFormulario.genero = parametros.genero;
-    modeloFormulario.telefono = parametros.telefono;
-    modeloFormulario.fechaNacimiento = fechaNacimientoo.toLocaleDateString();
-    modeloFormulario.carrera = parametros.carrera;
-    modeloFormulario.generoPoesia = parametros.generoPoesia;
-    modeloFormulario.fechaInscripcion = parametros.fechaInscripcion;
-    modeloFormulario.fechaDeclamacion = parametros.fechaDeclamacion;
+    if (carnet?.length === 6 && carnet?.includes('0') == '0') {
+        if (carnet?.charAt(0) === 'A' && carnet?.charAt(2) === '5') {
+            if (carnet.charAt(carnet.length - 1) === "1" && carnet.charAt(carnet.length - 1) === "3" && carnet.charAt(carnet.length - 1) === "9") {
+                if (parametros.carnet && parametros.nombre && parametros.direccion &&
+                    parametros.genero && parametros.telefono && parametros.fechaNacimiento && parametros.carrera && parametros.generoPoesia) {
+                    if (String(calcularEdad(parametros.fechaNacimiento)) > 17) {
 
-    if (cantidadLetras === 6) {
-        return res.status(500).send({ mensaje: 'Se necesitan 6 Caracteres para que sea valido' })
+                        modeloFormulario.carnet = parametros.carnet;
+                        modeloFormulario.nombre = parametros.nombre;
+                        modeloFormulario.direccion = parametros.direccion;
+                        modeloFormulario.genero = parametros.genero;
+                        modeloFormulario.telefono = parametros.telefono;
+                        modeloFormulario.fechaNacimiento = fechaNacimientoo.toLocaleDateString();
+                        modeloFormulario.carrera = parametros.carrera;
+                        modeloFormulario.generoPoesia = parametros.generoPoesia;
+                        modeloFormulario.fechaInscripcion = parametros.fechaInscripcion;
+                        modeloFormulario.fechaDeclamacion = parametros.fechaDeclamacion;
 
-    } else if (carnet.includes('0') == false) {
-        return res.status(500).send({ mensaje: 'No se permiten ceros' })
-
-    } else if (PrimeraLetra === 'A') {
-        return res.status(500).send({ mensaje: 'Debe ser A mayuscula el primer caracter' })
-
-    } else if (TerceraLetra === '5') {
-        return res.status(500).send({ mensaje: 'Debe ser 5 el Tercer Caracter caracter' })
-
-    } else if (SextaLetra === "1" || SextaLetra === "3" || SextaLetra === "9") {
-        return res.status(500).send({ mensaje: 'El Sexto Caracter debe ser 1, 3, 9' })
-
-    } else if (parametros.carnet && parametros.nombre && parametros.direccion &&
-        parametros.genero && parametros.telefono && parametros.fechaNacimiento && parametros.carrera && parametros.generoPoesia) {
-        return res.status(500).send({ mensaje: 'Agregue todos los parametros' })
-
-    } else if (String(calcularEdad(parametros.fechaNacimiento)) > 17) {
-        return res.status(500).send({ mensaje: 'Se necesita ser mayor de 17 años' })
-        
+                        modeloFormulario.save((err, formularioGuardado) => {
+                            if (err) return res.status(500).send({ mensaje: 'error en la peticion ' })
+                            if (!formularioGuardado) return res.status(500).send({ mensaje: 'error al crear formulario ' })
+                            return res.status(200).send(formularioGuardado.fechaDeclamacion)
+                        })
+                    } else {
+                        return res.status(500).send({ mensaje: "Necesitas ser mayor de 17aÃ±os" })
+                    }
+                } else {
+                    return res.status(500).send({ mensaje: "agregue todos los parametros" })
+                }
+            } else {
+                return res.status(500).send({ mensaje: "El Sexto Caracter debe ser '1, 3, 9' " })
+            }
+        } else {
+            return res.status(500).send({ mensaje: "Debe ser 'A mayuscula' el primer caracter,    El tercer caracter debe ser 5" })
+        }
     } else {
-
-
-
-        modeloFormulario.save((err, formularioGuardado) => {
-            if (err) return res.status(500).send({ mensaje: 'error en la peticion ' })
-            if (!formularioGuardado) return res.status(500).send({ mensaje: 'error al crear formulario ' })
-            return res.status(200).send(formularioGuardado.fechaDeclamacion)
-        })
+        return res.status(500).send({ mensaje: 'Se necesitan 6 Caracteres para que sea valido,     No se Permiten 0' })
     }
-
-
 }
+
+
+
+
+
 
 //----------------------------------------------------------------
 
