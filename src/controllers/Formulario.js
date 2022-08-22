4d3b66a2f1450e56e21699202337693552778282
Edwar Esaul López Pérez <88805623+elopez2020236@users.noreply.github.com>
2022-08-21 22:39:12 -0600

    Arreglo de errores ya casi

--- src/controllers/Formulario.js
+++ src/controllers/Formulario.js
@@ -1,5 +1,7 @@
 const Formulario = require('../models/Formulario.models');
 
+
+
 function calcularEdad(fechaNacimiento) {
     var fechaActual = new Date();
     var anoActual = parseInt(fechaActual.getFullYear());
@@ -20,16 +22,47 @@ function calcularEdad(fechaNacimiento) {
     return edad;
 };
 
+function diasDeLaSemana(inicio, final) {
+    var dia = new Date(inicio.getFullYear(),inicio.getMonth(),inicio.getDate() );
+    var i = 1;
+
+    while (final > 0) {
+        dia = new Date(inicio.getFullYear(),inicio.getMonth(),inicio.getDate() + i);
+
+        if (dia.getDay() > 0 && dia.getDay() < 6) {
+            final--;
+        }
+        i++;
+    }
+
+    return dia;
+}
+
+function diaDeLaSemana(Inicio, Final) {
+    var Dia = new Date(Inicio.getFullYear(),Inicio.getMonth() + 1,0);
+    var i = 1;
+  
+    while (Final == 0) {Dia = new Date(Inicio.getFullYear(),Inicio.getMonth() + 1,0 - i);
+  
+      if (Dia.getDay() > 0 && Dia.getDay() < 6) {
+        Final--;
+      }
+      i++;
+    }
+
+    return Dia;
+  }
 //----------------------------------------------------------------
 function GenerarFormulario(req, res) {
     var parametros = req.body;
     var modeloFormulario = new Formulario();
     var carnet = parametros.carnet
     var fechaNacimientoo = new Date(parametros.fechaNacimiento)
+    var generoPoesia1 = new Date();
 
     if (carnet?.length === 6 && carnet?.includes('0') == '0') {
         if (carnet?.charAt(0) === 'A' && carnet?.charAt(2) === '5') {
-            if (carnet.charAt(carnet.length - 1) === "1" && carnet.charAt(carnet.length - 1) === "3" && carnet.charAt(carnet.length - 1) === "9") {
+            if (carnet.charAt(carnet.length - 1) === "1" || carnet.charAt(carnet.length - 1) === "3" || carnet.charAt(carnet.length - 1) === "9") {
                 if (parametros.carnet && parametros.nombre && parametros.direccion &&
                     parametros.genero && parametros.telefono && parametros.fechaNacimiento && parametros.carrera && parametros.generoPoesia) {
                     if (String(calcularEdad(parametros.fechaNacimiento)) > 17) {
@@ -45,11 +78,32 @@ function GenerarFormulario(req, res) {
                         modeloFormulario.fechaInscripcion = parametros.fechaInscripcion;
                         modeloFormulario.fechaDeclamacion = parametros.fechaDeclamacion;
 
+                        if (carnet.charAt(carnet.length - 1) === '1' && parametros.generoPoesia == "dramatico") {
+                            const diaDelGeneroPoesia = diasDeLaSemana(generoPoesia1, 5);
+                            modeloFormulario.fechaDeclamacion = diaDelGeneroPoesia;
+
+                        } else if (carnet.charAt(carnet.length - 1) === '3' && parametros.generoPoesia == "epico") {
+                            const diaDelGeneroPoesia = diaDeLaSemana(generoPoesia1, );
+                            modeloFormulario.fechaDeclamacion = diaDelGeneroPoesia;
+
+                        } else {
+                            var Hoy = new Date(
+                                generoPoesia1.getFullYear(),
+                                generoPoesia1.getMonth(),
+                                generoPoesia1.getDate()
+                            );
+                            var SieteDias = Hoy.getDay() == 0 ? 7 : Hoy.getDay();
+                            var sinFines = 7 - SieteDias;
+                            Hoy.setDate(Hoy.getDate() + sinFines + 5);
+                            modeloFormulario.fechaDeclamacion = Hoy;
+                        }
+
                         modeloFormulario.save((err, formularioGuardado) => {
                             if (err) return res.status(500).send({ mensaje: 'error en la peticion ' })
                             if (!formularioGuardado) return res.status(500).send({ mensaje: 'error al crear formulario ' })
                             return res.status(200).send(formularioGuardado.fechaDeclamacion)
                         })
+
                     } else {
                         return res.status(500).send({ mensaje: "Necesitas ser mayor de 17aÃ±os" })
                     }
